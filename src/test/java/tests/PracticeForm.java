9c5d23ff30848f3d7ed695304c294001669d84d8
TyurinV <denfal486@gmailc.om>
2021-02-08 22:55:26 +0300

    Half of second homework

--- src/test/java/tests/PracticeForm.java
+++ src/test/java/tests/PracticeForm.java
@@ -3,13 +3,12 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static utils.RandomUtils.*;
+
 
 public class PracticeForm {
 
@@ -21,54 +20,60 @@ public class PracticeForm {
 
     @Test
     void textFields() {
+
         open("https://demoqa.com/automation-practice-form");
-        $(".main-header").shouldHave(text("Practice Form"));
-
-        $("#firstName").setValue("Фома");
-        $("#lastName").setValue("Киняев");
-        $("#userEmail").setValue("fedya@kinyaev.ru");
-
-        $("label[for='gender-radio-3']").click(); //gender other
-        $("#userNumber").setValue("5435434355");
-
-        // birthday
+        String firstName = getRandomString(10),
+                lastName = getRandomString(4),
+                email = getRandomEmail(),
+                gender = getRandomGender(1,1),
+                mobile = getRandomPhone(),
+                yearOfBirth = getRandomInt(1950, 2000),
+                monthOfBirth = "May",
+                dayOfBirth = getRandomInt(1, 28),
+                subject1 = "Hindi",
+                subject2 = "Commerce",
+                hobby1 = "Sports",
+                hobby2 = "Reading",
+                hobby3 = "Music",
+                picture = "che.png",
+                currentAddress = "г.Москва, ул.Академика Королева, д.12",
+                state = "Uttar Pradesh",
+                city = "Merrut";
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(mobile);
         $("#dateOfBirthInput").click();
-
-        $(".react-datepicker__month-select").selectOption("April"); // month
-        $(".react-datepicker__year-select").selectOption("1975");   // year
-        $("[aria-label='Choose Monday, April 14th, 1975']").click();      // day
-
-
-        $("#subjectsInput").setValue("hi").pressEnter();
-        $("label[for='hobbies-checkbox-1']").click(); //hobbies sports
-        $("label[for='hobbies-checkbox-2']").click(); //hobbies reading
-        $("label[for='hobbies-checkbox-3']").click(); //hobbies music
-
-        File file = new File("src/test/resources/che.png"); //file1 of 2
-        $("#uploadPicture").uploadFile(file);              //file2 of 2
-
-
-        $("#currentAddress").setValue("г.Москва, ул.Академика Королева, д.12");
-
-        $("#react-select-3-input").setValue("N").pressEnter();
-        $("#react-select-4-input").setValue("N").pressEnter();
-
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__day--0" + dayOfBirth).click();
+        $("#subjectsInput").setValue(subject1).pressEnter().setValue(subject2).pressEnter();
+        $(byText(hobby1)).click();
+        $(byText(hobby2)).click();
+        $(byText(hobby3)).click();
+        $("#uploadPicture").uploadFromClasspath("img/" + picture);
+        //File file = new File("src/test/resources/img/" + picture); //file1 of 2
+        //$("#uploadPicture").uploadFile(file);              //file2 of 2
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
         $("#submit").click();
-
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[1]/td[2]").shouldHave(text(firstName + " " + lastName));
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[2]/td[2]").shouldHave(text(email)); //mail
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[3]/td[2]").shouldHave(text(gender)); //gender
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[4]/td[2]").shouldHave(text(mobile)); // mobile
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[5]/td[2]").shouldHave(text(dayOfBirth+" "+ monthOfBirth + ","+ yearOfBirth)); // birth
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[6]/td[2]").shouldHave(text(subject1+ ", "+ subject2)); // Subjects
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[7]/td[2]").shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3)); // Hobbies
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[8]/td[2]").shouldHave(text(picture)); // picture
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[9]/td[2]").shouldHave(text("г.Москва, ул.Академика Королева, д.12")); //address
+        $x("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[10]/td[2]").shouldHave(text(state + " " + city)); //state and city
 
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[1]/td[2]")).shouldHave(text("Фома Киняев"));
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[2]/td[2]")).shouldHave(text("fedya@kinyaev.ru")); //mail
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[3]/td[2]")).shouldHave(text("Other")); //gender
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[4]/td[2]")).shouldHave(text("5435434355")); // mobile
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[5]/td[2]")).shouldHave(text("14 April,1975")); // birth
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[6]/td[2]")).shouldHave(text("Hindi")); // Subjects
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[7]/td[2]")).shouldHave(text("Sports, Reading, Music")); // Hobbies
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[8]/td[2]")).shouldHave(text("che.png")); // picture
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[9]/td[2]")).shouldHave(text("г.Москва, ул.Академика Королева, д.12")); //address
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[10]/td[2]")).shouldHave(text("NCR Gurgaon")); //state and city
-
-
+        sleep(10000);
     }
 
 
